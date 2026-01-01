acc0e9f8090106c7f5a40236e7e4ff58c2c13544
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-01 19:10:38 +0100

    Add files via upload

---
+++ lua/custom/plugins/cmdline.lua
@@ -0,0 +1,28 @@
+return {
+  "folke/noice.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    cmdline = {
+      view = "cmdline_popup",
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+        border = {
+          style = "rounded",
+        },
+      },
+    },
+  },
+}
+
---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,13 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  "lervag/vimtex",
+  lazy = false,     -- we don't want to lazy load VimTeX
+  -- tag = "v2.15", -- uncomment to pin to a specific release
+  init = function()
+    -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_view_method = "zathura"
+  end
+}
