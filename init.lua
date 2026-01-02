7bdff2962c36cfcf7b8d093f6fa61919233dbae5
Marie Bauernhuber <marie.bauernhuber@proton.me>
2026-01-02 13:20:05 +0100

    Fixed nvim-treesitter.config typo when initing

--- init.lua
+++ init.lua
@@ -795,8 +795,9 @@ require("lazy").setup({
 	},
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
+		main = "nvim-treesitter.config", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
 			ensure_installed = {
